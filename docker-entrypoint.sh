833574210052b052b5b8ef36d803e2d63407edbe
Antoine Brun <abr@ubiqube.com>
2025-06-17 14:33:45 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,7 +20,7 @@ then
 fi
 cd / || exit 1
 
-if [[ -f /opt/fmc_repository/cisco-ios-ms/.cloudclapp-dev ]]; then
+if [[ -f /opt/fmc_repository/cisco-ios-ms/.devops ]]; then
 	log_info "👾 Skipping upgrade for fellow developer."
 	exit 0
 fi
